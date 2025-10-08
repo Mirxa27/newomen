c8a4952e3543a3cf006e0da1a4779edfd9dd04e7
Abdullah Mirza <169343746+Mirxa27@users.noreply.github.com>
2025-10-08 13:28:48 +0300

    [dyad] Fixed Supabase function deployment error by moving type definitions. - wrote 4 file(s), deleted 1 file(s)

--- supabase/functions/realtime-token/index.ts
+++ supabase/functions/realtime-token/index.ts
@@ -1,5 +1,4 @@
-/// <reference lib="deno.ns" />
-/// <reference lib="deno.window" />
+/// <reference types="@deno/types" />
 
 import { serve } from "https://deno.land/std@0.190.0/http/server.ts";
 import "https://deno.land/x/xhr@0.1.0/mod.ts";
